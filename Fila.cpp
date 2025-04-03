991145adc111bce06753ded19881f1eb577003a4
Sophia Lima <sophialimasantos4@gmail.com>
2025-04-03 15:41:28 -0300

    questao fila

--- Fila.cpp
+++ Fila.cpp
@@ -1,65 +1,64 @@
 #include <iostream>
 using namespace std;
 #define MAX 10
- 
-public class FilaArray {
- 
-    private:
-        int final = -1;
-        int vetor[MAX];
- 
-    public:
-    FilaArray(int v) {
-        this.first = v;
-        this.vetor[0] = v;
-        this.final = v;
+
+class FilaArray {
+private:
+    int final = -1;
+    int vetor[MAX];
+
+public:
+    FilaArray() {} 
+
+    int getVetor(int p) {
+        return vetor[p];
     }
- 
-    int getVetor (int p) {
-        return this.vetor[int p];
+
+    void setVetor(int pos, int valor) {
+        vetor[pos] = valor;
     }
- 
-    void setVetor (int pos, int valor) {
-        this.vetor[pos] = valor;
+
+    void addNaFila(int numero) {
+        if (final == MAX - 1) {
+            cout << "Fila cheia";
+        } else {
+            final++;
+            vetor[final] = numero;
+            cout << "novo elemento adicionado: " << vetor[final] << endl;
+        }
     }
- 
-    void addNaFila (int numero) {
-        if(this.final == MAX - 1) cout << "Fila cheia";
-        else this.vetor[this.final++] = numero;
-    }
- 
+
     void removeDaFila() {
-        int i,n;
-        if(final == -1) {
+        if (final == -1) {
             cout << "Fila vazia.";
             return;
+        } else {
+            cout << "elemento removido: " << vetor[0] << endl;
+            for (int i = 0; i < final; i++) {
+                vetor[i] = vetor[i + 1];
+            }
+            final--;  
         }
-        else {
-            for(int i=0;i<final;i++){
-                this.vetor[i] = this.vetor[i+1];
-                this.final--;
-                
+    }
+
+    void listarFila() {
+        if (final == -1) {
+            cout << "Fila vazia.";
+            return;
+        } else {
+            cout << "Elementos na fila:" << endl;
+            for (int i = 0; i <= final; i++) {  
+                cout << vetor[i] << endl;
             }
         }
     }
- 
-    void listarFila () {
-        int i;
-        if(final == -1) {
-            cout << "Fila vazia.";
-            return;
-        }
-        else {
-            for(int i=0;i<=final;i++){
-                cout << this.vetor[i] << endl;
-            }
-        }
-    }
- 
-}
- 
-int main () {
- 
-    FilaArray novaFila = new FilaArray(5);
- 
+};
+
+int main() {
+    FilaArray novaFila;
+    novaFila.addNaFila(5);
+    novaFila.addNaFila(10);
+    novaFila.listarFila();
+    novaFila.removeDaFila();
+    novaFila.listarFila();
 }
