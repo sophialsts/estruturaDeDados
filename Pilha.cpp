12364b372b5a6af22a73feec2b2c6326e612fdab
Sophia Lima <sophialimasantos4@gmail.com>
2025-04-03 15:04:07 -0300

    questão pilha

--- Pilha.cpp
+++ Pilha.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
-#include <stack>
 using namespace std;
  
 class Pilha {
+    private:
     int tam, topo, v[10];
  
     public:
-    Pilha (int tamanho, int topo, int v[]);
+    Pilha (int tamanho, int topo) {
+        this->tam = tamanho;
+        this->topo = topo;
+    }
  
     void empilhar (int n) {
-        if(topo < tam-1){
-            topo++;
-            v[topo] = n;
+        if(topo <= tam-1){
+            this->topo++;
+            this->v[topo] = n;
+            this->tam++;
+           // cout << v[topo-1] << endl;
         }
         else {
             cout << "Pilha cheia" << endl;
         }
     }
  
-    int desempilhar ()  {
-        
+    void desempilhar ()  {
+        if(this->topo == -1){
+            cout << "Pilha vazia" << endl;
+        }
+        else {
+            this->topo--;
+            this->tam--;
+            /*não tem problema só decrementar porque o que estava presente no vetor vai ser sobrescrito pelos novos elementos empilhados*/
+        }
     };
 
-    void imprimir();
- 
+    void imprimir() {
+        for (int i=0;i<this->tam;i++) {
+            cout << this->v[i] << endl;
+        }
+    }
+
 };
+
+int main () {
+    Pilha novaPilha = Pilha(0, -1);
+    novaPilha.empilhar(1);
+    novaPilha.empilhar(2);
+    novaPilha.empilhar(3);
+    novaPilha.imprimir();
+
+}
